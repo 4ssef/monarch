acae88ec1262af360d94624ec999a246d6361108
unknown <fernandoassef@hotmail.com>
2023-06-26 02:46:30 -0600

    feat: asigna la lista de permisos .txt y los autoriza via administrador 'ms_graph_rsc_access_id.txt' y 'pbi_service_rsc_access_id.txt' contienen la lista de Access IDs separados por espacio, para que estos sean asignables a sus respectivas variables como una "lista".
    Ya que el formato para asignarse un permiso es {id}={type}, donde {id} es el resourceAccess.id y {type} resourceAccess.type, y todos los {id} compartían el mismo resourceAccess.type en común (Scope), en el permiso se concatenó el string "=Scope" en las líneas 24 y 29, pues pasar caractéres especiales como es el signo de igual '=' requiere más recursos de desarrollo.
    
    Esta función se optimizará en versiones futuras para una mayor portabilidad.

--- lib/add_permissions_to_app.bat
+++ lib/add_permissions_to_app.bat
@@ -9,11 +9,30 @@
 
 :: variables
 SET APP_ID=%1
+SET MS_GRAPH_API_ID=00000003-0000-0000-c000-000000000000
+SET PBI_SERVICE_API_ID=00000009-0000-0000-c000-000000000000
+SET /p MS_GRAPH_ACCESS_IDS=<%~dp0ms_graph_rsc_access_id.txt
+SET /p PBI_SERVICE_ACCESS_IDS=<%~dp0pbi_service_rsc_access_id.txt
 
 :: crea el servicio principal para la app
 CALL az ad sp create --id %APP_ID%
 
 :: concede los permisos de API
 
+:: asigna los 2 permisos de Microsoft Graph
+FOR %%i IN (%MS_GRAPH_ACCESS_IDS%) DO (
+	CALL az ad app permission add --id %APP_ID% --api %MS_GRAPH_API_ID% --api-permissions %%i=Scope
+)
+
+:: asigna los 22 permisos de Power BI Service
+FOR %%j IN (%PBI_SERVICE_ACCESS_IDS%) DO (
+	CALL az ad app permission add --id %APP_ID% --api %PBI_SERVICE_API_ID% --api-permissions %%j=Scope
+)
+
+:: espera 30 segundos a que se terminen de solicitar todos los permisos
+TIMEOUT /t 30 /nobreak
+
+:: autoriza via administrador, todos los permisos solicitados
+CALL az ad app permission admin-consent --id %APP_ID%
 
 EXIT
